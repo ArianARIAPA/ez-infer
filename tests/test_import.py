fac0636301fd6b9508d67ba06f11a8de6462d848
ArianARIAPA <ariapa.arian@gmail.com>
2022-07-23 23:13:12 +0430

    Added MxnetInfer.

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,4 +2,5 @@ def test_imports():
     from ezinfer.onnx_infer import OnnxInfer
     from ezinfer.pytorch_infer import PytorchInfer
     from ezinfer.tensorflow_infer import TensorflowInfer
+    from ezinfer.mxnet_infer import MxnetInfer
     assert 1 == 1
